9752e64724dc33eb10a6e2786503a915fee54d1d
Cory Alder <coryalder@gmail.com>
2026-03-08 11:29:39 -0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify';
+import SubsonicAPI from 'subsonic-api';
+
+const fastify = Fastify({
+  logger: true
+});
+
+// Basic route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world', subsonicApiLoaded: !!SubsonicAPI };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
